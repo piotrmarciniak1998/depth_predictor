93f733a340c4d74c0d62e3c41d3c7b142aaa000e
Piotr Marciniak <piotr.b.marciniak@student.put.poznan.pl>
2022-04-26 02:27:35 +0200

    small fix to create_dataset.py

--- create_dataset.py
+++ create_dataset.py
@@ -5,88 +5,90 @@ import zipfile
 
 USE_RGB = False
 DELETE_TEMP = True
-OCCLUSION_BOUNDS = (1, 20)
+OCCLUSION_BOUNDS = [(1, 20), (1, 40), (1, 60), (1, 80), (1, 99)]
 
-abs_path = os.path.abspath("./")
-input_dataset_path = f"{abs_path}/input_dataset"
-dataset_path = f"{abs_path}/dataset/{OCCLUSION_BOUNDS[0]}_{OCCLUSION_BOUNDS[1]}"
-temp_path = f"{abs_path}/temp"
+for occlusion_bound in OCCLUSION_BOUNDS:
+    abs_path = os.path.abspath("./")
+    input_dataset_path = f"{abs_path}/input_dataset"
+    dataset_path = f"{abs_path}/dataset/{occlusion_bound[0]}_{occlusion_bound[1]}"
+    temp_path = f"{abs_path}/temp"
 
-print(f"Started with occlusion bounds set as {OCCLUSION_BOUNDS}.")
+    print(f"Started with occlusion bound set as {occlusion_bound}.")
 
-try:
-    shutil.rmtree(dataset_path)
-except FileNotFoundError:
-    pass
-
-if DELETE_TEMP:
     try:
-        shutil.rmtree(f"{abs_path}/temp")
+        shutil.rmtree(dataset_path)
     except FileNotFoundError:
         pass
 
-os.makedirs(f"{dataset_path}/input", exist_ok=True)
-os.makedirs(f"{dataset_path}/ground_truth", exist_ok=True)
-os.makedirs(f"{temp_path}", exist_ok=True)
+    if DELETE_TEMP:
+        try:
+            shutil.rmtree(f"{abs_path}/temp")
+        except FileNotFoundError:
+            pass
 
-files = [f for f in os.listdir(input_dataset_path) if os.path.isfile(os.path.join(input_dataset_path, f))]
+    os.makedirs(f"{dataset_path}/input", exist_ok=True)
+    os.makedirs(f"{dataset_path}/ground_truth", exist_ok=True)
+    os.makedirs(f"{temp_path}", exist_ok=True)
 
-for i, file in enumerate(files):
-    with zipfile.ZipFile(f"{input_dataset_path}/{file}", 'r') as zip_ref:
-        print(f"[{i + 1}/{len(files)}] unzipping {file}.")
-        zip_ref.extractall(f"{temp_path}")
+    files = [f for f in os.listdir(input_dataset_path) if os.path.isfile(os.path.join(input_dataset_path, f))]
 
-last_index = 0
-global_index = 0
-all_files = 1
-for root, dirs, files in os.walk(temp_path):
-    if USE_RGB:
-        print(f"{global_index} pairs ready. \t"
-              f"{int(round(all_files / 2, 0))} pairs in current directory to check. \t"
-              f"{int(round(100 * global_index * 4 / all_files, 0))}% of files are used.")
-    else:
-        print(f"{global_index} pairs ready. \t"
-              f"{int(round(all_files / 2, 0))} pairs in current directory to check. \t"
-              f"{int(round(100 * global_index * 2 / all_files, 0))}% of files are used.")
+    for i, file in enumerate(files):
+        with zipfile.ZipFile(f"{input_dataset_path}/{file}", 'r') as zip_ref:
+            print(f"[{i + 1}/{len(files)}] unzipping {file}.")
+            zip_ref.extractall(f"{temp_path}")
 
-    for file in files:
-        file_path = os.path.join(root, file)
-        file_info = file.rstrip(".png").split("_")
-        file_index = int(file_info[0])
-        file_type = file_info[1]
-        file_content = file_info[2]
-        file_occlusion = int(file_info[3])
-
-        if not OCCLUSION_BOUNDS[0] <= file_occlusion <= OCCLUSION_BOUNDS[1]:
-            continue
-        elif not os.path.isfile(file_path):
-            continue
-        elif file_type == "rgb" and not USE_RGB:
-            continue
-
-        new_file_name = f"{global_index}_{file_type}_{file_occlusion}.png"
-
-        if file_content == "u":
-            new_file_path = f"{dataset_path}/input/"
-        elif file_content == "o":
-            new_file_path = f"{dataset_path}/ground_truth/"
+    last_index = 0
+    global_index = 0
+    all_files = 1
+    for root, dirs, files in os.walk(temp_path):
+        if USE_RGB:
+            print(f"{global_index} pairs ready. \t"
+                  f"{int(round(len(files) / 2, 0))} pairs in current directory to check. \t"
+                  f"{int(round(100 * global_index * 4 / all_files, 0))}% of files are used.")
         else:
-            new_file_path = None
+            print(f"{global_index} pairs ready. \t"
+                  f"{int(round(len(files) / 2, 0))} pairs in current directory to check. \t"
+                  f"{int(round(100 * global_index * 2 / all_files, 0))}% of files are used.")
 
-        shutil.move(file_path, new_file_path)
-        os.rename(f"{new_file_path}/{file}", f"{new_file_path}/{new_file_name}")
+        for file in files:
+            file_path = os.path.join(root, file)
+            file_info = file.rstrip(".png").split("_")
+            file_index = int(file_info[0])
+            file_type = file_info[1]
+            file_content = file_info[2]
+            file_occlusion = int(file_info[3])
 
-        if last_index != file_index:
-            global_index += 1
-            last_index = file_index
+            if not occlusion_bound[0] <= file_occlusion <= occlusion_bound[1]:
+                continue
+            elif not os.path.isfile(file_path):
+                continue
+            elif file_type == "rgb" and not USE_RGB:
+                continue
 
-    all_files += len(files)
+            new_file_name = f"{global_index}_{file_type}_{file_occlusion}.png"
 
-if DELETE_TEMP:
-    print("Deleting temporary directory.")
-    try:
-        shutil.rmtree(f"{abs_path}/temp")
-    except FileNotFoundError:
-        pass
+            if file_content == "o":
+                new_file_path = f"{dataset_path}/input/"
+            elif file_content == "u":
+                new_file_path = f"{dataset_path}/ground_truth/"
+            else:
+                new_file_path = None
 
-print(f"Created {global_index} pairs of images. Total number of pairs is {int(round(all_files / 2, 0))}.")
+            shutil.move(file_path, new_file_path)
+            os.rename(f"{new_file_path}/{file}", f"{new_file_path}/{new_file_name}")
+
+            if last_index != file_index:
+                global_index += 1
+                last_index = file_index
+
+        all_files += len(files)
+
+    if DELETE_TEMP:
+        print("Deleting temporary directory.")
+        try:
+            shutil.rmtree(f"{abs_path}/temp")
+        except FileNotFoundError:
+            pass
+
+    print(f"Created {global_index} pairs of images. Total number of pairs is {int(round(all_files / 2, 0))}.")
+print("Done.")
